7bab248533d977bdfa3a5a67222289f92cea9f08
Kerem Kaya <kerem@mgssoftware.net>
2022-10-29 23:51:35 +0300

    events and variables created

--- contracts/contracts/DeAuth.sol
+++ contracts/contracts/DeAuth.sol
@@ -1,7 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 // Uncomment this line to use console.log
 import "hardhat/console.sol";
 
-contract DeAuth {}
+contract DeAuth {
+    event AddNewPassword(
+        address indexed from,
+        uint256 timestamp,
+        string socialMedia,
+        string walletApp
+    );
+    event Disconnect(
+        address indexed from,
+        uint256 timestamp,
+        string socialMedia,
+        string walletApp
+    );
+
+    event MoveConnectedSocialMediaToNewWallet(
+        address fromOld,
+        address indexed fromNew,
+        uint256 timestamp,
+        string socialMedia,
+        string walletApp
+    );
+    event changePassword(
+        address indexed from,
+        uint256 timestamp,
+        string socialMedia,
+        string walletApp
+    );
+
+    //Social medias that have passwords from DeAuth
+    mapping(address => mapping(string => string[])) ConnectedSocialMedias;
+
+    mapping(address => mapping(string => uint256[])) currentSaltValues;
+
+    constructor() {}
+}
